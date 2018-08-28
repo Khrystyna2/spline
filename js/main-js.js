180874b04e8c7f8b73d83e63ca72ba539aa2bfbc
Khrystyns Khozhai <l.xrustja@gmail.com>
2018-08-28 15:41:27 +0300

    change sub-menu

--- js/main-js.js
+++ js/main-js.js
@@ -4,6 +4,13 @@ $(document).ready(function() {
 		$('#ru, #ua').toggleClass('change-language__active');
 	});
 
+	$('.menu-btn-catalog').click(function() {
+		$('.menu-catalog').slideToggle(1000);
+	});
+	$('.menu-btn-services').click(function() {
+		$('.menu-services').slideToggle(1000);
+	});
+
 	$('.filter').click(function() {
 		$('.filter-category').slideToggle( "slow" );
 	});
