e6d4d985cb4cb784b9448c9eb3946f80cf8aea95
Khrystyns Khozhai <l.xrustja@gmail.com>
2018-08-29 10:12:30 +0300

    madified menu

--- js/main-js.js
+++ js/main-js.js
@@ -4,17 +4,32 @@ $(document).ready(function() {
 		$('#ru, #ua').toggleClass('change-language__active');
 	});
 
-	$('.menu-btn-catalog').click(function() {
-		$('.menu-catalog').slideToggle(600);
-	});
-	$('.menu-btn-services').click(function() {
-		$('.menu-services').slideToggle(600);
-	});
+
+		if ($(window).width() <= 960) {
+			$('.menu-btn-catalog').click(function() {
+				$('.menu-catalog').slideToggle(600);
+			});
+			$('.menu-btn-services').click(function() {
+				$('.menu-services').slideToggle(600);
+			});
+		}
+	   else {
+		$('.menu-btn-catalog').hover(function() {
+			$('.menu-catalog').slideToggle(300);
+		});
+		$('.menu-btn-services').hover(function() {
+			$('.menu-services').slideToggle(300);
+		});
+	   }
+	// $('.menu-btn-catalog').click(function() {
+	// 	$('.menu-catalog').slideToggle(600);
+	// });
+	// $('.menu-btn-services').click(function() {
+	// 	$('.menu-services').slideToggle(600);
+	// });
 
 	$('.filter').click(function() {
 		$('.filter-category').slideToggle( "slow" );
 	});
 
-
-	$('select').styler();
 });
