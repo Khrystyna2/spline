a141fec89eeec426f495087f366fbebf6dd6444a
Khrystyns Khozhai <l.xrustja@gmail.com>
2018-08-30 13:09:39 +0300

    hover effect

--- js/main-js.js
+++ js/main-js.js
@@ -5,44 +5,61 @@ $(document).ready(function() {
 	});
 
 
-		if ($(window).width() <= 960) {
-			$('.menu-btn-catalog').click(function() {
-				$('.menu-catalog').slideToggle(600);
-			});
-			$('.menu-btn-services').click(function() {
-				$('.menu-services').slideToggle(600);
-			});
-		}
-	   else {
-		$('.menu-btn-catalog').hover(function() {
-			$('.menu-catalog').toggleClass('open');
-		});
-		$('.menu-btn-services').hover(function() {
-			$('.menu-services').toggleClass('open');
-		});
-	   }
-	// $('.menu-btn-catalog').click(function() {
-	// 	$('.menu-catalog').slideToggle(600);
-	// });
-	// $('.menu-btn-services').click(function() {
-	// 	$('.menu-services').slideToggle(600);
-	// });
+	// 	if ($(window).width() <= 960) {
+	// 		$('.menu-btn-catalog').click(function() {
+	// 			$('.menu-catalog').slideToggle(600);
+	// 		});
+	// 		$('.menu-btn-services').click(function() {
+	// 			$('.menu-services').slideToggle(600);
+	// 		});
+	// 	}
+	//    else {
+	// 	$('.menu-btn-catalog').hover(function() {
+	// 		$('.menu-catalog').toggleClass('open');
+	// 	});
+	// 	$('.menu-btn-services').hover(function() {
+	// 		$('.menu-services').toggleClass('open');
+	// 	});
+	//    }
+	
+	if ($(window).width() > 1024) {
+        $('.menu-btn-catalog').hover(function () {
+			$('.menu-catalog').fadeIn(200);
+        }, function () {
+            $('.menu-catalog').fadeOut(200);
+        });
+	}
+	
+    if ($(window).width() <= 1024) {
+        $('.menu-btn-catalog').click(function () {
+            $('.menu-catalog').slideToggle(600);
+        });
+	}
+	
+	if ($(window).width() > 1024) {
+        $('.menu-btn-services').hover(function () {
+			$('.menu-services').fadeIn(200);
+        }, function () {
+            $('.menu-services').fadeOut(200);
+        });
+	}
+	
+    if ($(window).width() <= 1024) {
+        $('.menu-btn-services').click(function () {
+            $('.menu-services').slideToggle(600);
+        });
+    }
+
 
 	$('.filter').click(function() {
 		$('.filter-category').slideToggle( "slow" );
 	});
 
+	//style for celect
 	$('select').styler();
 
 
-
-	// $('.cards-products__img-small').slick({
-	// 	infinite: true,
-	// 	slidesToShow: 3,
-	// 	slidesToScroll: 1
-	//   });
-
-
+	//slick slider
 	  $('.cards-products__img').slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
